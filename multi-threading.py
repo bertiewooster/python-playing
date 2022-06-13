702d113184a957c89968e81bd494c793c8a21987
Jeremy Monat <jemonat@calalum.org>
2022-06-12 22:47:00 -0400

    Add a thread

--- multi-threading.py
+++ multi-threading.py
@@ -1,13 +1,18 @@
 # From https://realpython.com/lessons/threading-python-overview/
 
+import threading
 import time
 
 
-def myfunc():
-    print("hello")
+def myfunc(name):
+    print(f"myfunc started with {name}")
     time.sleep(5)
+    print(f"myfunc ended")
     return True
 
+
 if __name__ == "__main__":
-    myfunc()
-    
+    print(f"main started")
+    t = threading.Thread(target=myfunc, args=["realpython"])
+    t.start()
+    print(f"main ended")
